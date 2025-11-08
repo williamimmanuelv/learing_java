f4581bcab1d27f735b59831fa1f0b9f1b28a5b2f
williamimmanuelv <williamimmanuelv.com>
2025-11-08 13:15:38 +0530

    string comparison

--- DataTypes.java
+++ DataTypes.java
@@ -1,9 +1,8 @@
 import java.lang.System;
 import java.util.Scanner;
-
 class DataTypes {
-    public static void main(String arg[])
-    {
+
+    public static void main(String arg[]) {
         // boolean rain = true;
 
         // if(true){
@@ -12,23 +11,35 @@ class DataTypes {
         // else{
         //     System.out.print("Enjoy the sunlight");
         // }
+        // Scanner scan = new Scanner(System.in);
+        // int num1 = scan.nextInt();
+        // int num2 = scan.nextInt();
+        // if(num1 > num2){
+        //     System.out.print("number 1 is greater");
+        // }
+        // else if(num1 < num2){
+        //     System.out.print("Number 2 is Greater");
+        // }
+        // else if(num1 == num2){
+        //     System.out.print("Both Numbers are equal");
+        // }
+        // String s1 = new String("apple");
+        // String s2 = new String("apple");
+        // String s3 = "orange";
+        // String s4 = "orange";
+        // System.out.println(s1.equals(s2));
+        // System.out.print(s3 == s4);
 
-        Scanner scan = new Scanner(System.in);
+        // // practice
+        // String a = "one";
+        // String b = "one";
+        // String c = b;
 
-        
-        int num1 = scan.nextInt();
-        int num2 = scan.nextInt();
+        // System.out.print(a==c);
 
-        if(num1 > num2){
-            System.out.print("number 1 is greater");
-        }
-        else if(num1 < num2){
-            System.out.print("Number 2 is Greater");
-        }
-        else if(num1 == num2){
-            System.out.print("Both Numbers are equal");
-        }
-    
-    
+        // String a = "one";
+        // String b = new String("one");
+        // String c = b;
+        // System.out.print(a==c);
     }
 }
