9b9079ba060ae286ae587141261521727710542f
williamimmanuelv <williamimmanuelv@gmail.com>
2025-11-13 22:32:04 +0530

    Fix test commit after Email correction

--- For_loop.java
+++ For_loop.java
@@ -31,11 +31,11 @@ class For_loop {
         // }
 
         
-        // for (int j = 1; j <= 10; j = j + 1) {
-        //     for (int i = 1; i <= j; i = i + 1) {
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+        for (int j = 1; j <= 10; j = j + 1) {
+            for (int i = 1; i <= j; i = i + 1) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
     }
 }
